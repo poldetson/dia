2d73f9c36363760ed75f7530e2e7e346fedc9c79
poldetson <su7484@gmail.com>
2020-07-19 20:20:09 +0900

    station names change to real.

--- dia.py
+++ dia.py
@@ -3,81 +3,94 @@
 
 #import sys
 import csv
-import tkinter as tk
+import tkinter
 
+# graphical value
 DOT_SIZE = 10
+CANVAS_SIZE_HEIGHT = 1500
+CANVAS_SIZE_WIDTH = 1500
+# draw base lines
+baseline_top = 100
+baseline_left = 200
+baseline_right = baseline_left + 60 * (23-5)
 
+# Data
+#stations = [['A', 100], ['B', 200], ['C', 0]]
 stations = []
-csvfile = open("station.txt", encoding="utf-8")
-for row in csv.reader(csvfile):
-#    print(row)
-    station = [row[0], int(row[1])]
-    stations.append(station)
-print('stations:')
-print(stations)
+train = []
+
+# Func: station data input
+def station_read(stations):
+    csvfile = open("station.txt", encoding="utf-8")
+    for row in csv.reader(csvfile):
+        station = [row[0], int(row[1])]
+        stations.append(station)
 
 
-times = []
-csvfile = open("dots.txt", encoding="utf-8")
-for row in csv.reader(csvfile):
-#    print(row)
-    time = []
-    for column in row:
-#        print(column)
-        time.append(int(column))
-    times.append(time)
-print('times:')
-print(times)
+# Func: train data input
+def train_read():
+    csvfile = open("train.txt", encoding="utf-8")
+    for row in csv.reader(csvfile):
+        time = []
+        for column in row:
+            time.append(int(column))
+        train.append(time)
 
-root = tk.Tk()
-root.title(u"Software Title")
-#root.geometry("400x300")
+# display core
+def disp_stations():
+    line = baseline_top
+    for hour in range(5, 24):
+        canvas.create_text(baseline_left+(hour-5)*60, line-20, text = format(hour, '02'), font=("Helvetica", 18, "bold"))
+    for station in stations:
+        station_name = station[0]
+        line = station[1]
+        canvas.create_text(baseline_left-80, line, text = station_name, font=("Helvetica", 18, "bold"))
+        canvas.create_line(baseline_left, line, baseline_right, line, width=3.0, fill='black')
 
-canvas = tk.Canvas(root, height=600, width=1000)
+def disp_train():
+    # draw lines
+    flag = 0
+    i = 0
+    for time in train:
+        if flag == 0:
+            flag = 1
+            last_station = stations[i][1]
+            last_time = time
+            i = i + 1
+            continue
+        station = stations[i][1]
+        print('%' + str(i) + ': ' + str(last_time) + ', ' + str(last_station) + ', ' + str(time) + ', ' + str(station))
+        canvas.create_line(last_time, last_station, time, station, width=3.0, fill='red')
+        last_station = station
+        last_time = time
+        i = i + 1
+    # draw dots
+    i = 0
+    for time in train:
+        canvas.create_oval(time[0] - DOT_SIZE/2, stations[i][1] - DOT_SIZE/2, time[0] + DOT_SIZE/2, stations[i][1] + DOT_SIZE/2 , fill='blue')
+        i = i + 1
 
 # canvas commands
 # create_rectangle(x0, y0, x1, y1, **options)
 
-# draw base lines
-baseline_top = 100
-baseline_left = 100
-baseline_right = 400
-#stations = [['A', 100], ['B', 200], ['C', 0]]
+## main 
 
-line = baseline_top
-for station in stations:
-    station_name = station[0]
-    line = station[1]
-    canvas.create_text(baseline_left-15, line, text = station_name, font=("Helvetica", 18, "bold"))
-    canvas.create_line(baseline_left, line, baseline_right, line, width=3.0, fill='black')
+root = tkinter.Tk()
+root.title(u"dia")
+#root.geometry("400x300")
+canvas = tkinter.Canvas(root, height=CANVAS_SIZE_HEIGHT, width=CANVAS_SIZE_WIDTH)
 
-# draw lines
-flag = 0
-i = 0
-for time in times:
-#    print('time:' + str(time))
-    if flag == 0:
-        flag = 1
-        last_station = stations[i][1]
-        last_time = time
-        i = i + 1
-        continue
-    station = stations[i][1]
-    print('%' + str(i) + ': ' + str(last_time) + ', ' + str(last_station) + ', ' + str(time) + ', ' + str(station))
-    canvas.create_line(last_time, last_station, time, station, width=3.0, fill='red')
-    last_station = station
-    last_time = time
-    i = i + 1
+station_read(stations)
+#print("stations")
+#print(stations)
 
-# draw dots
-i = 0
-for time in times:
-#    print('time:' + str(time[0]))
-#    print('station[i][1]:' + str(stations[i][1]))
-    canvas.create_oval(time[0] - DOT_SIZE/2, stations[i][1] - DOT_SIZE/2, time[0] + DOT_SIZE/2, stations[i][1] + DOT_SIZE/2 , fill='blue')
-    i = i + 1
+train_read()
+#print(train)
+
+disp_stations()
+
+disp_train()
 
 canvas.pack()
-
 root.mainloop()
 
