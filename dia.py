e3f34c50df8c5fbd1cfe16728915bff7eb1afa17
poldetson <su7484@gmail.com>
2020-07-22 21:39:04 +0900

    enable to display multi trains.

--- dia.py
+++ dia.py
@@ -6,7 +6,7 @@ import csv
 import tkinter
 import datetime
 
-# graphical value
+# graphical constant value
 DOT_SIZE = 10
 CANVAS_SIZE_HEIGHT = 1500
 CANVAS_SIZE_WIDTH = 1500
@@ -17,49 +17,48 @@ baseline_right = baseline_left + 60 * (23-5)
 
 # Data
 #stations = [['A', 100], ['B', 200], ['C', 0]]
-stations = []
-train = []  # [name, timetable = [[Num, time], [Num, time], .. [Num, time]]
+#train = []  # [name, timetable = [[Num, time], [Num, time], .. [Num, time]]
 
 # Func: station data input
 def station_read(stations):
-    print('station_read')
+    #print('station_read')
     csvfile = open("station.txt", encoding="utf-8")
     for row in csv.reader(csvfile):
         station = [row[0], int(row[1])]
         stations.append(station)
 
 # Func: train data input
-def train_read(train):
-    print('train_read')
+def train_read(trains):
+    train = []
+    #print('train_read')
     flag = 0
     origin_time_dt = datetime.datetime.strptime('00:00', '%H:%M')
-    print('origin_time_dt')
+    #print('origin_time_dt')
     print(origin_time_dt)
     minute_in_seconds = datetime.timedelta(minutes=1)
-    print('minute_in_seconds')
+    #print('minute_in_seconds')
     print(minute_in_seconds)
     csvfile = open("train.txt", encoding="utf-8") 
     for row in csv.reader(csvfile):
-        print(row)
-        if flag == 0:
-            train.append(row)   # as Train name
-            flag = 1
+        #print(row)
+        if row[0] == "END":
+            trains.append(train)
+            train = []
+            flag = 0
         else:
-            train_num = int(row[0])
-            print('train_num')
-            print(train_num)
-            time_at_station = datetime.datetime.strptime(row[1], ' %H:%M')
-            td = time_at_station - origin_time_dt
-            print('td')
-            print(td)
-            train_time = int(td / minute_in_seconds)
-            print('train_time')
-            print(train_time)
-            train.append([train_num, train_time])
+            if flag == 0:
+                train.append(row)   # as Train name
+                flag = 1
+            else:
+                train_num = int(row[0])
+                time_at_station = datetime.datetime.strptime(row[1], ' %H:%M')
+                td = time_at_station - origin_time_dt
+                train_time = int(td / minute_in_seconds)
+                train.append([train_num, train_time])
 
 # display core
 def disp_stations():
-    print('disp_station')
+    #print('disp_station')
     line = baseline_top
     for hour in range(5, 24):
         canvas.create_text(baseline_left+(hour-5)*60, line-20, text = format(hour, '02'), font=("Helvetica", 18, "bold"))
@@ -69,36 +68,39 @@ def disp_stations():
         canvas.create_text(baseline_left-80, line, text = station_name, font=("Helvetica", 18, "bold"))
         canvas.create_line(baseline_left, line, baseline_right, line, width=3.0, fill='black')
 
-def disp_train(train):
-    print('disp_train')
+def disp_train(trains):
+    #print('disp_train')
     # draw lines
-    flag = 0 
-    for tmp in train:
-        if flag == 0:
-            flag = 1
-            train_name = tmp
-        elif flag == 1:
-            flag = 2
-            last_station = stations[int(tmp[0])][1]
-            last_time = tmp[1]
-        else:
-            station = stations[int(tmp[0])][1]
-            time = tmp[1]
-            canvas.create_line(last_time, last_station, time, station, width=3.0, fill='red')
-            last_station = station
-            last_time = time
+    for train in trains:
+        flag = 0 
+        for tmp in train:
+            if flag == 0:
+                flag = 1
+                train_name = tmp
+            elif flag == 1:
+                flag = 2
+                last_station = stations[int(tmp[0])][1]
+                last_time = tmp[1]
+            else:
+                station = stations[int(tmp[0])][1]
+                time = tmp[1]
+                canvas.create_line(last_time, last_station, time, station, width=3.0, fill='red')
+                last_station = station
+                last_time = time
     # draw dots
-    flag = 0
-    for tmp in train:
-        if flag == 0:
-            flag = 1
-        else:
-            canvas.create_oval(tmp[1] - DOT_SIZE/2, stations[int(tmp[0])][1] - DOT_SIZE/2, tmp[1] + DOT_SIZE/2, stations[int(tmp[0])][1] + DOT_SIZE/2 , fill='blue')
+        flag = 0
+        for tmp in train:
+            if flag == 0:
+                flag = 1
+            else:
+                canvas.create_oval(tmp[1] - DOT_SIZE/2, stations[int(tmp[0])][1] - DOT_SIZE/2, tmp[1] + DOT_SIZE/2, stations[int(tmp[0])][1] + DOT_SIZE/2 , fill='blue')
 
 # canvas commands
 # create_rectangle(x0, y0, x1, y1, **options)
 
 ## main 
+stations = []
+trains = [] # [train, train, train, ..., train]
 
 root = tkinter.Tk()
 root.title(u"dia")
@@ -109,12 +111,12 @@ station_read(stations)
 #print("stations")
 #print(stations)
 
-train_read(train)
-print(train)
+train_read(trains)
+#print(train)
 
 disp_stations()
 
-disp_train(train)
+disp_train(trains)
 
 canvas.pack()
 root.mainloop()
